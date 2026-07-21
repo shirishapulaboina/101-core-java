59084d32ff7868ab5b4aa5a7a593314590095e92
shirishapulaboina <shirishashirisha2698@gmail.com>
2026-07-21 21:31:28 +0530

    package

---
+++ src/com/main/studentApp.java
@@ -0,0 +1,14 @@
+package com.main;
+import com.student.Student;
+public class studentApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student obj=new Student();
+		obj.setDetails();
+		obj.displayDetails();
+
+
+	}
+
+}
---
+++ src/com/student/Student.java
@@ -0,0 +1,27 @@
+package com.student;
+
+public class Student {
+
+	int id;
+	String name;
+	String course;
+	public void setDetails() {
+		 id=10;
+		 name="Rahul";
+		 course="Java Full Stack";
+	}
+	public void displayDetails() {
+		System.out.println("ID  :"+id);
+		System.out.println("Name  :"+name);
+		System.out.println("ID  :"+course);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Student obj=new Student();
+//		obj.setDetails();
+//		obj.displayDetails();
+
+	}
+
+}
